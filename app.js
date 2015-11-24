fe6217d138e8aa100e69d6501578e909ac908a4f
Daniel Moreira <danielmoreira@sioslife.com>
2015-11-24 15:58:56 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var app = express();
+var io = require('socket.io').listen(server);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('listening at http://%s:%s', host, port);
+});
+
+var io = require('socket.io').listen(server);
+
+var clients = [];
+function Client (id){
+    this.socketId = id;
+    this.userId = "";
+    this.name = "";
+}
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log(' socket - ' + socket.id + ' - joined.');
+
+    var client = new Client(socket.id);
+    clients.push(client);
+
+	socket.on('set-name', function (data) {
+		console.log(' socket - ' + socket.id + ' - change name to - ' + data.name + ' - .');
+
+		client.name = data.name;
+        client.room = data.room;
+        socket.join( client.room );
+        socket.broadcast.to(client.room).emit('new-client', { name : data.name, room : data.room, socketId : socket.id });
+        socket.emit('clients', { clients : getClientsInRoom(client.room, client.name)});
+        //console.log(getClientsInRoom(client.room, client.name));
+	});
+
+    socket.on('call', function (data) {
+        var user = getClient(data.name);
+        if(user){
+            socket.broadcast.to(user.socketId).emit( 'call', {from: socket.id} );
+            console.log("Socket id: "+socket.id+" connecting in: "+socketId);
+        }
+        else{
+            socket.emit( 'call-response', {status: 3} );
+        }
+	});
+
+    socket.on('call-response', function (data) {
+        console.log(data);
+        var to = data.to;
+        data.from = socket.id;
+        delete data.to;
+        socket.broadcast.to(to).emit('call-response', data);
+	});
+
+    socket.on('end-call', function (data) {
+        var socketId = null;
+        clients.forEach(function(c, i){
+            if( data.targetUser === c.name ){
+                socketId = c.socketId;
+            }
+        });
+
+        if(socketId!==null){
+            // console.log("Emiting "+JSON.stringify({from: socket.id})+" to: "+socketId);
+            socket.broadcast.to(socketId).emit( 'end-call', {from: socket.id} );
+        }
+	});
+
+    socket.on('send-message', function (data) {
+        console.log(data);
+        var to = data.to;
+        data.from = socket.id;
+        delete data.to;
+        socket.broadcast.to(to).emit('message', data);
+    });
+
+	socket.on('disconnect', function ()  {
+		console.log(' disconnect client - ' + socket.id + ' - ' );
+        var client = null;
+        clients.forEach(function(c, i){
+            if(c.socketId === socket.id){
+                client = c;
+                return;
+            }
+        });
+        if( client !== null ){
+            clients.splice(clients.indexOf(client), 1);
+            socket.broadcast.to(client.room).emit('clients', { clients : clients});
+        }
+	});
+});
+
+function getClientsInRoom(room, name){
+    var aux = [];
+    clients.forEach(function(c, i){
+        if( room === c.room && name !== c.name ){
+            aux.push(c);
+        }
+    });
+    return aux;
+}
+
+function getClient(name){
+    clients.forEach(function(c, i){
+        if( name !== c.name ){
+            return c;
+        }
+    });
+    return undefined;
+}
+
+setInterval(function() { 
+    console.log("CLIENTS : " + clients.length);
+    //console.log(clients);
+}, 10000);
---
+++ public/main.js
@@ -0,0 +1,160 @@
+var constraints = { video : true, audio : true };
+
+var getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia).bind(navigator);
+var PeerConnection = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
+var MediaStream = window.webkitMediaStream || window.mozMediaStream || window.MediaStream;
+var pc;
+var socket = io();
+var isInitiator;
+var from;
+var configuration = {
+  'iceServers': [{
+    'url': 'stun:8490.s1.minisipserver.com'
+  }]
+};
+
+function call ( id ) {
+    isInitiator = true;
+    socket.emit('call', { name : id });
+    //start( id );
+}
+
+function start( id ) {
+    from = id;
+    pc = new PeerConnection(configuration);
+
+    pc.onicecandidate = function (evt) {
+        if (evt.candidate){
+            socket.emit('send-message', {type : 'candidate', candidate : evt.candidate, to : id});
+        }
+    };
+
+    pc.onnegotiationneeded = function () {
+        pc.createOffer(function (desc) {
+            pc.setLocalDescription(desc, function () {
+                socket.emit('send-message', {type : 'sdp', sdp : desc, to : id});
+            });
+        });
+    }
+
+    pc.onaddstream = function(evt) {
+        var video = document.getElementById('remote-video');
+        video.src = window.URL.createObjectURL(evt.stream);
+    }
+
+    getUserMedia(constraints, successCallBack, errorCallBack);
+}
+
+socket.on('message', function (data) {
+
+    if(!pc) {
+        isInitiator = false;
+        start(data.from);
+    }
+    console.log("message");
+    console.log(data);
+
+    if (data.type == 'offer') {
+        console.log('offer');
+        pc.setRemoteDescription(new SessionDescription(data.sdp));   
+    }
+
+    if(data.type == 'answer'){
+        console.log('answer');
+        pc.setRemoteDescription(new SessionDescription(data.sdp));
+    }
+    if (data.type == 'candidate'){
+        pc.addIceCandidate(new IceCandidate(data.candidate));
+    }
+});
+
+function endCall() {
+    pc.close();
+}
+
+function error(err) {
+    console.log(err);
+}
+
+function successCallBack (stream) {
+    var video = document.getElementById('local-video');
+    video.src = window.URL.createObjectURL(stream);
+    pc.addStream(stream);
+
+    if(isInitiator) {
+        pc.createOffer(function (desc) {
+            console.log("create offer");
+            console.log(desc);
+            pc.setLocalDescription(desc);
+            socket.emit('send-message', {type : 'offer', sdp : desc, to : from});
+        }, error);
+    }else {
+        pc.createAnswer(function (desc) {
+            console.log("create answer");
+            console.log(desc);
+            pc.setLocalDescription(desc);
+            socket.emit('send-message', {type : 'answer', sdp : desc, to : from});
+        }, error);
+    }
+}
+
+function errorCallBack (error) {
+    console.log("getUserMedia error: ", error);
+};
+
+function sendName(){
+    var name = document.getElementById("iUsername").value;
+    var room = document.getElementById("iRoom").value;
+    socket.emit("set-name", { name : name, room : room });
+}
+
+socket.on('clients', function (data) {
+    var lista = document.getElementById("lista");
+    lista.innerHTML = "";
+    for (var i = 0; i < data.clients.length; i++) {
+        appendItem(lista, data.clients[i].room, data.clients[i].name, data.clients[i].socketId);
+    }
+});
+
+socket.on('new-client', function (data) {
+    var lista = document.getElementById("lista");
+    appendItem(lista, data.room, data.name, data.socketId);
+});
+
+socket.on('call-response', function (data) {
+    var message = getMessage(data.result);
+    alert(message);
+    if(data.result == 1){
+        start(data.from);
+    }
+});
+
+socket.on('call', function (data) {
+    console.log(data);
+    if(confirm("Um user pretende-lhe ligar.")){
+        socket.emit('call-response', { to : data.from, result : 1});
+    }else {
+        socket.emit('call-response', { to: data.from, result : 2});
+    }
+});
+
+function appendItem( lista, room, name, id ) {
+    var item = document.createElement("LI");
+    item.innerHTML = "<li>"+room + " - " + name+"<button onclick='call(" + '"'+ name + '"' + ")'> Call </button></li>";
+    lista.appendChild(item);
+}
+
+function getMesssage(result){
+    switch(result){
+        case 1:
+            return "Chamada aceite.";
+        case 2:
+            return "Chamada recusada.";
+        case 3:
+            return "Utilizador offline";
+        default:
+            return "Codigo invalido"; 
+    }
+}
